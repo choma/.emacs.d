9f6722410c74966362ed0edc945375ebcab3e9f2
Matías Croce <mati@nelumboweb.com.ar>
2015-09-27 19:09:00 -0300

    Redefine web-mode indentation

--- init.el
+++ init.el
@@ -312,7 +312,10 @@
 (setq web-mode-enable-current-column-highlight t) ;; highlight current col
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2)
+  (setq indent-tabs-mode t)
+  (setq-default tab-width 4)
+  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-markup-indent-offset 4)
   )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
