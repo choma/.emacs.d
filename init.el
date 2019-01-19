935bcd112fa7b733c884e056989c047213a3a5a2
Matías Croce <mati@nelumboweb.com.ar>
2019-01-19 16:58:04 -0300

    Stop using marmalade repo

--- init.el
+++ init.el
@@ -41,7 +41,6 @@
 ;; Add package sources
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("marmalade" . "https://marmalade-repo.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
 			 ))
 (package-initialize)
